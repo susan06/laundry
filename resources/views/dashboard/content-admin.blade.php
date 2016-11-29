7c48501c78e5fdb3a39646a8b7289e034d9be8c4
Dayannis <dayannis.y@gmail.com>
2016-11-28 20:46:41 -0400

    Corrección en Home - Admin

--- resources/views/dashboard/content-admin.blade.php
+++ resources/views/dashboard/content-admin.blade.php
@@ -23,8 +23,8 @@
             <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
               <span class="count_top"><i class="fa fa-user"></i> Total Connections</span>
               <div class="count">7,325</div>
-            </div>
-          </div>-->
+            </div>-->
+          </div>
           <!-- /top tiles -->
 
           
